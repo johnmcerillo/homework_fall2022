b95d247918c1f8cd34088f615e5d45208e845e88
John <johnmcerillo@gmail.com>
2023-07-04 16:13:43 -0400

    DQN todos

--- hw3/cs285/policies/argmax_policy.py
+++ hw3/cs285/policies/argmax_policy.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 class ArgMaxPolicy(object):
 
     def __init__(self, critic):
@@ -11,7 +10,10 @@ class ArgMaxPolicy(object):
             observation = obs
         else:
             observation = obs[None]
-        
-        ## TODO return the action that maxinmizes the Q-value 
+
+        qa_values = self.critic.qa_values(observation)
+        action = np.argmax(qa_values, -1)
+
+        ## TODO return the action that maximizes the Q-value
         # at the current observation as the output
         return action.squeeze()
